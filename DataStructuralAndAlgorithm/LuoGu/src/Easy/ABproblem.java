16f70bd45f798e9fe5da1db32b3ecff5ae3db8d5
yingzi2345 <140298351+yingzi2345@users.noreply.github.com>
2026-07-24 16:31:51 +0800

    First put

---
+++ DataStructuralAndAlgorithm/LuoGu/src/Easy/ABproblem.java
@@ -0,0 +1,18 @@
+package Easy;
+
+import java.util.Scanner;
+
+public class ABproblem {
+    public static void main(String[] args) {
+        //输入两个整数 a,b，输出它们的和。
+        //
+        //注意：
+        //
+        //Pascal 使用 integer 会爆掉哦！
+        //有负数哦！
+        //C/C++ 的 main 函数必须是 int 类型。程序正常结束时的返回值必须是 0。这不仅对洛谷其他题目有效，而且也是 NOIP/CSP/NOI 比赛的要求！
+        Scanner number = new Scanner(System.in);
+        int a = number.nextInt(),b=number.nextInt();
+        System.out.println(a+b);
+    }
+}
