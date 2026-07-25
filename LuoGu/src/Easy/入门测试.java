b6340d57735bede5a14ef1c10e3752a2e489e071
YLong <YLong@233.com>
2026-07-25 22:10:47 +0800

    update:依旧是A+B和二分查找第二遍，顺便让它返回下标的写法。

---
+++ LuoGu/src/Easy/入门测试.java
@@ -0,0 +1,12 @@
+package Easy;
+
+import java.util.Scanner;
+
+public class 入门测试 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        System.out.println(a + b);
+    }
+}
--- study/src/binary_search.java
+++ study/src/binary_search.java
@@ -1,8 +1,11 @@
+import javax.swing.text.Highlighter;
+
 public class binary_search {
     public static Integer binarySearch(int[] arr, int item) {
-        //二分查找
+
+        //二分查找*2
         int low = 0;
-        int high = arr.length - 1;//-1
+        int high = arr.length - 1;//因为是下标，所以-1
 
         while (low <= high) {
             // 计算中间下标，整除
@@ -12,10 +15,10 @@ public class binary_search {
                 return guess;
             }
             if (guess > item) {
-                // 中间值偏大，往左半边找
+                //中间值偏大,往小的找
                 high = mid - 1;
             } else {
-                // 中间值偏小，往右半边找
+                //否则中间值偏小，往大的找
                 low = mid + 1;
             }
         }
@@ -23,8 +26,30 @@ public class binary_search {
     }
 
     public static void main(String[] args) {
-        int[] my_list = {1, 3, 5, 7, 9};
-        System.out.println(binarySearch(my_list,3));
-        System.out.println(binarySearch(my_list,-1));
+        int[] my_list = {1, 3, 5, 7, 9};//数组必须是有序的
+        System.out.println(binarySearch2(my_list, 3));
+        System.out.println(binarySearch2(my_list, -1));
     }
+
+
+    //----------------返回下标的形式-------------------
+    public static int binarySearch2(int[] arr, int itme) {
+        int lo = 0;
+        int h = arr.length - 1;
+        while (lo <= h) {
+            int midd = (lo + h) / 2;
+            int guesss = arr[midd];
+            if (guesss == itme) {
+                return midd;
+            } else if (guesss > itme) {
+                h = midd - 1;
+            } else {
+                lo = midd + 1;
+            }
+
+        }
+        return -1;
+    }
+
+
 }
